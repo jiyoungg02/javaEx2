c6a1ddaa2d5ca26cc43eec12365981243b9e7c75
jiyoungg02 <jiyoung_g02@naver.com>
2025-04-07 14:13:11 +0900

    Interface

---
+++ src/ch08/unit06/Ex03.java
@@ -0,0 +1,29 @@
+package ch08.unit06;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		Demo3 t = new Test3();
+		t.print();
+		
+	}
+}
+
+interface Demo3{
+	public void print();
+	public void disp();
+}
+
+// 추상클래스는 인터페이스의 모든 메소드를 재정의하지 않아도 된다.
+abstract class DemoImpl3 implements Demo3{
+	@Override
+	public void print() {
+		System.out.println("print...");
+	}
+}
+
+class Test3 extends DemoImpl3{
+	@Override
+	public void disp() {
+		System.out.println("disp...");
+	}
+}
---
+++ src/ch08/unit07/Ex02.java
@@ -0,0 +1,43 @@
+package ch08.unit07;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		// Test2.User2 obj = new Test2.User2(); // 컴오류
+		
+		Test2 t = new Test2();
+		
+		// 외부 클래스의 객체를 이용하여 객체 생성
+		Test2.User2 obj = t.new User2(); 
+			// 이런 형식으로는 잘 사용하지 않는다.
+			// 일반적으로 외부클래스(Test2) 내의 메소드에서 객체를 생성하여 사용
+			// 보안에 유리
+		obj.write();
+		
+	}
+}
+
+class Test2 {
+	int a= 10;
+	static int b = 20;
+	
+	// 내부클래스 : 외부 클래스의 객체가 생성되어야 객체를 생성할 수 있는 클래스
+	class User2{
+		int x = 100;
+		
+		public void write() {
+			System.out.println(a + "," + b + "," + x);
+			// print(); // 가능
+				// 즉, 외부 클래스 객체가 생성되어야 내부 클래스 사용이 가능하기 때문에 print() 사용 가능
+		}
+	}
+	
+	public void print() {
+		System.out.println("print...");
+	}
+	
+	public void sub() {
+		// 일반적으로 메소드 내에서 내부클래스의 객체 생성 및 활용
+		User2 u = new User2();
+		u.write();
+	}
+}
